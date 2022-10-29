c9ef06e7a308dd70a1c6c43087f561876393ba76
desilva-se19047 <92102903+desilva-se19047@users.noreply.github.com>
2022-10-29 22:18:26 +0530

    Add files via upload

---
+++ kmain.c
@@ -0,0 +1,14 @@
+    #include "drivers/frame_buffer.h"
+    #include "drivers/serial_port.h"
+
+
+    
+    int main(){
+
+           char ptr2[] = "Welcome to LittleOS";
+   
+    
+    serial_write(0x3F8, ptr2, 19);
+    fb_write(ptr2, 19);
+    
+    }
